98731e10cf4a6f8eba34ffea7720b1a4c63c5007
Roman Blum <rmnblm@gmail.com>
2016-09-26 10:55:38 +0200

    WIP: implement Filtering (there is currently a problem when enabling a filter again)

--- src/Examples/FilteringViewController.swift
+++ src/Examples/FilteringViewController.swift
@@ -9,15 +9,17 @@
 import UIKit
 import Mapbox
 
-class FilteringViewController: UIViewController, MGLMapViewDelegate {
+struct Filter {
+    let title: String
+    var layer: MGLStyleLayer
+}
+
+class FilteringViewController: UIViewController, UITableViewDelegate, UITableViewDataSource,  MGLMapViewDelegate {
+    
+    var filters = [Filter]()
     
     @IBOutlet var mapView: MGLMapView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        addFilterButton()
-    }
+    @IBOutlet var tableView: UITableView!
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -31,32 +33,61 @@ class FilteringViewController: UIViewController, MGLMapViewDelegate {
         let geoJSONSource = MGLGeoJSONSource(sourceIdentifier: "sv", url: geoJSONURL)
         mapView.style().add(geoJSONSource)
         
-        let cafeStyleLayer = MGLCircleStyleLayer(layerIdentifier: "sv-cafe-layer", sourceIdentifier: "sv")!
-        cafeStyleLayer.predicate = NSPredicate(format: "amenity == 'cafe'")
-        cafeStyleLayer.circleColor = UIColor.brown
-        mapView.style().add(cafeStyleLayer)
+        addLayerWithFilter(title: "Cafes", color: UIColor.brown, predicate: NSPredicate(format: "amenity == 'cafe'"))
+        addLayerWithFilter(title: "Banks", color: UIColor.green, predicate: NSPredicate(format: "amenity == 'bank'"))
+        addLayerWithFilter(title: "Toilets", color: UIColor.white, predicate: NSPredicate(format: "amenity == 'toilets'"))
+        addLayerWithFilter(title: "Restaurants", color: UIColor.blue, predicate: NSPredicate(format: "amenity == 'restaurant'"))
         
-        let bankStyleLayer = MGLCircleStyleLayer(layerIdentifier: "sv-bank-layer", sourceIdentifier: "sv")!
-        bankStyleLayer.predicate = NSPredicate(format: "amenity == 'bank'")
-        bankStyleLayer.circleColor = UIColor.green
-        mapView.style().add(bankStyleLayer)
-        
-        let toiletsStyleLayer = MGLCircleStyleLayer(layerIdentifier: "sv-toilets-layer", sourceIdentifier: "sv")!
-        toiletsStyleLayer.predicate = NSPredicate(format: "amenity == 'toilets'")
-        toiletsStyleLayer.circleColor = UIColor.black
-        mapView.style().add(toiletsStyleLayer)
-        
-        let restaurantStyleLayer = MGLCircleStyleLayer(layerIdentifier: "sv-restaurant-layer", sourceIdentifier: "sv")!
-        restaurantStyleLayer.predicate = NSPredicate(format: "amenity == 'restaurant'")
-        restaurantStyleLayer.circleColor = UIColor.blue
-        mapView.style().add(restaurantStyleLayer)
+        tableView.reloadData()
     }
     
-    private func addFilterButton() {
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterTapped))
+    private func addLayerWithFilter(title: String, color: UIColor, predicate: NSPredicate) {
+        let styleLayer = MGLCircleStyleLayer(layerIdentifier: "sv-" + title + "-layer", sourceIdentifier: "sv")!
+        styleLayer.predicate = predicate
+        styleLayer.circleColor = color
+        mapView.style().add(styleLayer)
+        
+        let filter = Filter(title: title, layer: styleLayer)
+        filters.append(filter)
     }
     
-    func filterTapped() {
+    // MARK: UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filters.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? FilterViewCell else {fatalError()}
         
+        let filter = filters[(indexPath as NSIndexPath).row]
+        cell.titleLabel.text  = filter.title
+        cell.stateSwitch.addTarget(self, action: #selector(filterStateChanged), for: UIControlEvents.valueChanged)
+        cell.stateSwitch.tag = indexPath.row
+        
+        return cell
+    }
+    
+    func filterStateChanged(sender: UISwitch) {
+        let filter = filters[sender.tag]
+        if (sender.isOn) {
+            mapView.style().add(filter.layer)
+        } else {
+            mapView.style().remove(filter.layer)
+        }
+    }
+    
+    // MARK: UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
---
+++ src/FilterViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FilteringViewCell.swift
+//  Mapbox-iOS-Examples
+//
+//  Created by Roman Blum on 25.09.16.
+//  Copyright © 2016 rmnblm. All rights reserved.
+//
+
+import UIKit
+
+class FilterViewCell : UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var stateSwitch: UISwitch!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
