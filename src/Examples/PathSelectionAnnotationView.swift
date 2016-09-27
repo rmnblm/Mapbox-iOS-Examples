1e66e82f807b3badc75538a4b78d106b0533359c
Roman Blum <rmnblm@gmail.com>
2016-09-27 21:42:50 +0200

    add invisible points to make the PolyLine interactive

--- src/Examples/PathSelectionAnnotationView.swift
+++ src/Examples/PathSelectionAnnotationView.swift
@@ -2,7 +2,7 @@
 //  PathSelectionAnnotationView.swift
 //  Mapbox-iOS-Examples
 //
-//  Created by Phil Schilter on 26.09.16.
+//  Created by Roman Blum on 27.09.16.
 //  Copyright © 2016 rmnblm. All rights reserved.
 //
 
@@ -18,17 +18,9 @@ class PathSelectionAnnotationView: MGLAnnotationView {
         
         // Use CALayer’s corner radius to turn this view into a circle.
         layer.cornerRadius = frame.width / 2
-        layer.borderWidth = 2
-        layer.borderColor = UIColor.white.cgColor
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        
-        // Animate the border width in/out, creating an iris effect.
-        let animation = CABasicAnimation(keyPath: "borderWidth")
-        animation.duration = 0.1
-        layer.borderWidth = selected ? frame.width / 4 : 2
-        layer.add(animation, forKey: "borderWidth")
     }
 }
--- src/Examples/PathSelectionViewController.swift
+++ src/Examples/PathSelectionViewController.swift
@@ -15,10 +15,24 @@ class PathSelectionViewController: UIViewController, MGLMapViewDelegate {
     @IBOutlet var mapView: MGLMapView!
     
     public func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
-        drawPolyline()
+        drawPoints()
+        drawLines()
     }
     
-    private func drawPolyline() {
+    private func drawLines() {
+        
+        let geoJSONURL = Bundle.main.url(forResource: "portland", withExtension: "geojson")!
+        
+        let geoJSONSource = MGLGeoJSONSource(sourceIdentifier: "portland", url: geoJSONURL)
+        mapView.style().add(geoJSONSource)
+        
+        let styleLayer = MGLLineStyleLayer(layerIdentifier: "portland-layer", sourceIdentifier: "portland")!
+        styleLayer.lineWidth = 5 as MGLStyleAttributeValue!
+        styleLayer.lineColor = UIColor.blue as MGLStyleAttributeValue!
+        mapView.style().add(styleLayer)
+    }
+    
+    private func drawPoints() {
         DispatchQueue.global().async { [unowned self] in
             do {
                 let jsonPathUrl = Bundle.main.url(forResource: "portland", withExtension: "geojson")
@@ -33,26 +47,11 @@ class PathSelectionViewController: UIViewController, MGLMapViewDelegate {
                         for location in feature["geometry"]["coordinates"].arrayValue {
                             let coordinate = CLLocationCoordinate2DMake(location[1].doubleValue, location[0].doubleValue)
                             coordinates.append(coordinate)
-                            
-                            
                         }
                         
-                        
-                        let line = MGLPolyline(coordinates: &coordinates, count: UInt(coordinates.count))
-                        line.title = feature["properties"]["name"].stringValue
-                        
-                        DispatchQueue.main.async {
-                            // Unowned reference to self to prevent retain cycle
-                            [unowned self] in
-                            self.mapView.addAnnotation(line)
-                            
-                            for coordinate in coordinates {
-                                let point = MGLPointAnnotation()
-                                point.coordinate = coordinate
-                                point.title = "Marker selected"
-                                point.subtitle = String(coordinate.latitude) + "/" + String(coordinate.longitude)
-                                self.mapView.addAnnotation(point)
-                            }
+                        for (index, _) in coordinates.enumerated() {
+                            if index == 0 { continue } // skip first
+                            self.split(coordinates[index - 1], coordinates[index])
                         }
                     }
                 }
@@ -64,25 +63,55 @@ class PathSelectionViewController: UIViewController, MGLMapViewDelegate {
         }
     }
     
+    private func add(coordinate: CLLocationCoordinate2D) {
+        DispatchQueue.main.async {
+            // Unowned reference to self to prevent retain cycle
+            [unowned self] in
+            let point = MGLPointAnnotation()
+            point.coordinate = coordinate
+            point.title = "Marker selected"
+            point.subtitle = "\(coordinate.latitude) / \(coordinate.longitude)"
+            self.mapView.addAnnotation(point)
+        }
+    }
+    
+    private func split(_ from: CLLocationCoordinate2D, _ to: CLLocationCoordinate2D) {
+        if distance(from, to) > 200 { // THRESHOLD is 200m
+            let middle = mid(from, to)
+            add(coordinate: middle)
+            split(from, middle)
+            split(middle, to)
+        }
+        
+        add(coordinate: from)
+        add(coordinate: to)
+    }
+    
+    private func distance(_ from: CLLocationCoordinate2D, _ to: CLLocationCoordinate2D) -> Double {
+        let fromLoc = CLLocation(latitude: from.latitude, longitude: from.longitude)
+        let toLoc = CLLocation(latitude: to.latitude, longitude: to.longitude)
+        return fromLoc.distance(from: toLoc)
+    }
+    
+    private func mid(_ from: CLLocationCoordinate2D, _ to: CLLocationCoordinate2D) -> CLLocationCoordinate2D {
+        let latitude = (from.latitude + to.latitude) / 2
+        let longitude = (from.longitude + to.longitude) / 2
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
     // MARK: - MGLMapViewDelegate methods
     
     func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
-        
-        guard annotation is MGLPointAnnotation else {
-            return nil
-        }
-        
-        let reuseIdentifier = String(annotation.coordinate.latitude) + "/" + String(annotation.coordinate.longitude)
+        let reuseIdentifier = annotation.subtitle!!
         
         // For better performance, always try to reuse existing annotations
         var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier)
         
         // If there’s no reusable annotation view available, initialize a new one
         if annotationView == nil {
-            annotationView = MarkerSelectFeedbackAnnotationView(reuseIdentifier: reuseIdentifier)
-            annotationView!.frame = CGRect.init(x: 0, y: 0, width: 8, height: 8)
-            
-            annotationView!.backgroundColor = UIColor.blue
+            annotationView = PathSelectionAnnotationView(reuseIdentifier: reuseIdentifier)
+            annotationView!.frame = CGRect.init(x: 0, y: 0, width: 12, height: 12)
+            annotationView!.backgroundColor = UIColor.clear
         }
         
         return annotationView
@@ -90,9 +119,5 @@ class PathSelectionViewController: UIViewController, MGLMapViewDelegate {
     
     public func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
         return true
-    }
-    
-    public func mapView(_ mapView: MGLMapView, lineWidthForPolylineAnnotation annotation: MGLPolyline) -> CGFloat {
-        return 5
     }
 }
