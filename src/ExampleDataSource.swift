ac01517e9d30f725d059c4ea5861195b1073843a
Roman Blum <rmnblm@gmail.com>
2016-10-03 15:57:17 +0200

    using UITapGestureRecognizer to detect path selection

--- src/ExampleDataSource.swift
+++ src/ExampleDataSource.swift
@@ -67,11 +67,6 @@ struct ExampleDataSource {
             title: "Heatmap",
             detail: "Display a heatmap of earthquakes in America using clustering and predicates.",
             classPrefix: "Heatmap"
-        ),
-        Example(
-            title: "Mapbox Studio",
-            detail: "Accessing data and styles remotely from Mapbox Studio (change Style URL in code first).",
-            classPrefix: "MapboxStudio"
         )
     ]
 }
--- src/Examples/PathSelectionViewController.swift
+++ src/Examples/PathSelectionViewController.swift
@@ -8,19 +8,51 @@
 
 import UIKit
 import Mapbox
-import SwiftyJSON
 
 class PathSelectionViewController: UIViewController, MGLMapViewDelegate {
     
     @IBOutlet var mapView: MGLMapView!
     
-    public func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
-        drawPoints()
-        drawLines()
+    var annotation: MGLPointAnnotation?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        mapView.addGestureRecognizer(tapGestureRecognizer)
     }
     
-    private func drawLines() {
+    func handleTap(recognizer: UITapGestureRecognizer) {
+        if annotation != nil {
+            mapView.removeAnnotation(annotation!)
+        }
         
+        let location = recognizer.location(in: mapView)
+        let touchableSquare = squareFrom(location: location)
+        for feature in mapView.visibleFeatures(in: touchableSquare, styleLayerIdentifiers: ["portland-layer"]) {
+            
+            print(feature)
+            print(feature.attributes)
+            
+            if feature is MGLPolylineFeature {
+                let lineFeature = feature as! MGLPolylineFeature
+                
+                annotation = MGLPointAnnotation()
+                annotation!.coordinate = lineFeature.coordinate
+                annotation!.title = lineFeature.title
+                annotation!.subtitle = lineFeature.subtitle
+                mapView.addAnnotation(annotation!)
+                return
+            }
+        }
+    }
+    
+    private func squareFrom(location: CGPoint) -> CGRect {
+        let length = 50.0
+        return CGRect(x: Double(location.x - CGFloat(length / 2)), y: Double(location.y - CGFloat(length / 2)), width: length, height: length)
+    }
+    
+    public func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
         let geoJSONURL = Bundle.main.url(forResource: "portland", withExtension: "geojson")!
         
         let geoJSONSource = MGLGeoJSONSource(sourceIdentifier: "portland", url: geoJSONURL)
@@ -32,90 +64,7 @@ class PathSelectionViewController: UIViewController, MGLMapViewDelegate {
         mapView.style().add(styleLayer)
     }
     
-    private func drawPoints() {
-        DispatchQueue.global().async { [unowned self] in
-            do {
-                let jsonPathUrl = Bundle.main.url(forResource: "portland", withExtension: "geojson")
-                let jsonData = try Data(contentsOf: jsonPathUrl!)
-                let json = JSON(data: jsonData)
-                
-                for feature in json["features"].arrayValue {
-                    if feature["geometry"]["type"].stringValue == "LineString" {
-                        
-                        var coordinates = [CLLocationCoordinate2D]()
-                        
-                        for location in feature["geometry"]["coordinates"].arrayValue {
-                            let coordinate = CLLocationCoordinate2DMake(location[1].doubleValue, location[0].doubleValue)
-                            coordinates.append(coordinate)
-                        }
-                        
-                        for (index, _) in coordinates.enumerated() {
-                            if index == 0 { continue } // skip first
-                            self.split(coordinates[index - 1], coordinates[index])
-                        }
-                    }
-                }
-            }
-            catch
-            {
-                print("GeoJSON parsing failed")
-            }
-        }
-    }
-    
-    private func add(coordinate: CLLocationCoordinate2D) {
-        DispatchQueue.main.async {
-            // Unowned reference to self to prevent retain cycle
-            [unowned self] in
-            let point = MGLPointAnnotation()
-            point.coordinate = coordinate
-            point.title = "Marker selected"
-            point.subtitle = "\(coordinate.latitude) / \(coordinate.longitude)"
-            self.mapView.addAnnotation(point)
-        }
-    }
-    
-    private func split(_ from: CLLocationCoordinate2D, _ to: CLLocationCoordinate2D) {
-        if distance(from, to) > 200 { // THRESHOLD is 200m
-            let middle = mid(from, to)
-            add(coordinate: middle)
-            split(from, middle)
-            split(middle, to)
-        }
-        
-        add(coordinate: from)
-        add(coordinate: to)
-    }
-    
-    private func distance(_ from: CLLocationCoordinate2D, _ to: CLLocationCoordinate2D) -> Double {
-        let fromLoc = CLLocation(latitude: from.latitude, longitude: from.longitude)
-        let toLoc = CLLocation(latitude: to.latitude, longitude: to.longitude)
-        return fromLoc.distance(from: toLoc)
-    }
-    
-    private func mid(_ from: CLLocationCoordinate2D, _ to: CLLocationCoordinate2D) -> CLLocationCoordinate2D {
-        let latitude = (from.latitude + to.latitude) / 2
-        let longitude = (from.longitude + to.longitude) / 2
-        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-    }
-    
     // MARK: - MGLMapViewDelegate methods
-    
-    func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
-        let reuseIdentifier = annotation.subtitle!!
-        
-        // For better performance, always try to reuse existing annotations
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier)
-        
-        // If there’s no reusable annotation view available, initialize a new one
-        if annotationView == nil {
-            annotationView = PathSelectionAnnotationView(reuseIdentifier: reuseIdentifier)
-            annotationView!.frame = CGRect.init(x: 0, y: 0, width: 12, height: 12)
-            annotationView!.backgroundColor = UIColor.clear
-        }
-        
-        return annotationView
-    }
     
     public func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
         return true
